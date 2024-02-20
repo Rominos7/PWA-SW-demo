dd0d652c34981e21a6320dd253d7bfddeab7c658
Rominos7 <Rominos7@gmail.com>
2024-02-20 22:26:45 +0200

    adding_new_util

---
+++ src/utils/messageLog.js
@@ -0,0 +1,11 @@
+export const messageLog = (message) => {
+    //eslint-disable-next-line no-console
+    console.log(message);
+};
+
+export const errorMessageLog = (error) => {
+    //eslint-disable-next-line no-console
+    console.error(error);
+}
+
+export default messageLog;
